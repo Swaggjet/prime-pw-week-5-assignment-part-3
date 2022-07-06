a23b63497b12f1596a473fb6bc575a9ca1ae895b
Swaggjet <asantesimmons@gmail.com>
2022-07-06 10:41:03 -0500

    week5pt3

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,14 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished, tracks) {
+  let goodAlbum = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+    tracks: tracks
+  }
+  collection.push(goodAlbum);
+  return goodAlbum;
+}
