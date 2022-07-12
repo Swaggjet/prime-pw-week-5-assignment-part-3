07dff7658aa913d0138e1205b528ea288057e6b3
Swaggjet <asantesimmons@gmail.com>
2022-07-11 23:17:04 -0500

    week5pt3

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,12 +3,48 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 function addToCollection(title, artist, yearPublished, tracks) {
-  let goodAlbum = {
+  let awesomeAlbum = {
     title: title,
     artist: artist,
     yearPublished: yearPublished,
     tracks: tracks
   }
-  collection.push(goodAlbum);
-  return goodAlbum;
+  collection.push(awesomeAlbum);
+  return awesomeAlbum;
 }
+
+console.log('Adding album to collection:', addToCollection(
+  'Cool people'
+  , 'Mac Miller'
+  , '2015',
+  [ 
+    {
+      name: 'Intro',
+      length: '3:00'
+    },
+    {
+      name: 'I\'m a cool person',
+      length: '3:00'
+    },
+  ]
+)
+
+console.log('Adding album to collection:', addToCollection(
+  'Indigo'
+  , 'Chris Brown'
+  , '2019',
+  [ 
+    {
+      name: 'Burgendy',
+      length: '3:20'
+    },
+    {
+      name: 'Il',
+      length: '3:00'
+    },
+  ]
+
+
+
+)
+)
