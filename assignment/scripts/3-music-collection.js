1b688d296f0a9d4fc4c1a12a274929c867d9ff67
Swaggjet <asantesimmons@gmail.com>
2022-07-12 00:02:35 -0500

    week5pt3

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,7 +2,7 @@ console.log('***** Music Collection *****')
 let collection = []; //create a varible to allow collection be an open array
 function addToCollection(titles, artist, yearPublished){ //Set up a function and set the paramenters for the album
 
-//set up a object 'album' with the needed parameters needed within the object.
+//set up a object 'album' to store the album information
   let album = {
     titles: titles,
     artist: artist,
@@ -12,4 +12,23 @@ function addToCollection(titles, artist, yearPublished){ //Set up a function and
   collection.push(album);
 return console.log('now added to the collection',  album.artist);
 }
+
+//Added music to the collection
+addToCollection('Jazz', 'John Coltrane', '1958');
+addToCollection('Kids', 'Kids', '2000');
+addToCollection('Drake', 'Drake', '2016');
+addToCollection('Time & Space', 'Kid Cudi', 2020)
+addToCollection('Theory of Everything', 'Everything', '2001')
+addToCollection(Black eye peas, 'Eminem', '2002')
+console.log(collection);
+
+function showCollection(array){
+  console.log(array.length);
+  for (let i = 0; i<array.length; i++){
+    console.log('Album selected:',`${array[i].titles}, by ${array[i].artist}, published in ${array[i].yearPublished}`);
+  }
+}
+showCollection(collection);
+
+
 
